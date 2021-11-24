1a02355ce19ccfe99e70320255a63279d7c568f8
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-11-25 04:07:41 +0900

    geonwoo victory

---
+++ kim/Assets/script/treasure.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class treasure : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Destroy(collision.gameObject);
+            SceneManager.LoadScene("Vitory");
+        }
+    }
+}

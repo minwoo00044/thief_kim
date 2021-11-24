50a45ca789dee41bfdb26f24dab511834763f081
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-11-24 18:30:41 +0900

    geonwoo make ui

---
+++ kim/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnclickNewGame()
+    {
+        Debug.Log("새 게임");
+    }
+
+    public void OnclickLoad()
+    {
+        Debug.Log("불러오기");
+    }
+
+    public void OnClickOption()
+    {
+        Debug.Log("옵션");
+    }
+
+    public void OnclickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
